6e7ef6ce208fca8ed4043d6a78428b0c61cbd613
David T. Sadler <davidtsadler@googlemail.com>
2020-05-29 13:13:00 +0100

    Put instructions into correct place

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Gohan
+
+## Installation:
+
+On an Arch based distribution as root, run the following:
+
+```
+curl -LO https://raw.githubusercontent.com/davidtsadler/gohan/master/gohan.sh
+sh gohan.sh
+```
--- gohan.sh
+++ gohan.sh
@@ -2,12 +2,3 @@
 # My Arch Linux installation script 
 # by David T. Sadler <davidtsadler@googlemail.com>
 # License: GNU GPLv3
-
-## Installation:
-
-On an Arch based distribution as root, run the following:
-
-```
-curl -LO https://raw.githubusercontent.com/davidtsadler/gohan/master/gohan.sh
-sh goha.sh
-```
