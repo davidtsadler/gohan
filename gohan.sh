01181b2f642189f6ced8e1c510dc116138ba34b2
David T. Sadler <davidtsadler@googlemail.com>
2020-05-29 13:19:02 +0100

    Install dialog as it will be required by the install script

--- gohan.sh
+++ gohan.sh
@@ -2,3 +2,14 @@
 # My Arch Linux installation script 
 # by David T. Sadler <davidtsadler@googlemail.com>
 # License: GNU GPLv3
+
+### FUNCTIONS ###
+
+installpkg(){ pacman --noconfirm --needed -S "$1" >/dev/null 2>&1 ;}
+
+### THE ACTUAL SCRIPT ###
+
+### This is how everything happens in an intuitive format and order.
+
+# Check if user is root on Arch distro. Install dialog.
+installpkg dialog || error "Are you sure you're running this as the root user and have an internet connection?"
