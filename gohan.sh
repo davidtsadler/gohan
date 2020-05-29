4f01dcb1221bbf9515b1e7200ebcfabf66feb76f
David T. Sadler <davidtsadler@googlemail.com>
2020-05-29 13:09:53 +0100

    Add installation instructions

--- gohan.sh
+++ gohan.sh
@@ -2,3 +2,12 @@
 # My Arch Linux installation script 
 # by David T. Sadler <davidtsadler@googlemail.com>
 # License: GNU GPLv3
+
+## Installation:
+
+On an Arch based distribution as root, run the following:
+
+```
+curl -LO https://raw.githubusercontent.com/davidtsadler/gohan/master/gohan.sh
+sh goha.sh
+```
